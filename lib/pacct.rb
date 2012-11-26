c864a54619447e6e302490ae56fa93d93b070a0a
Ben Merritt <blm768@gmail.com>
2012-11-26 14:20:45 -0800

    Initial commit

---
+++ lib/pacct.rb
@@ -0,0 +1,12 @@
+require "pacct/version"
+
+require "pacct/pacct_c"
+
+#Contains classes for working with process accounting files in acct(5) format
+module Pacct
+  class Entry
+   #  def end_date
+#       return
+#     end
+  end
+end
---
+++ lib/pacct/version.rb
@@ -0,0 +1,4 @@
+module Pacct
+  #The library version
+  VERSION = "0.8.0"
+end
